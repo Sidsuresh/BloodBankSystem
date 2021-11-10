3001f0caa09d3f7bbb485fac91a743341f0c4262
Sidsuresh <60881487+Sidsuresh@users.noreply.github.com>
2021-11-10 16:55:36 +0400

    Added Login and Styling

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,12 +1,39 @@
-// import './LandingPage.css'
+import './Login.css'
+import {useNavigate} from 'react-router-dom'
 
 const Login = () =>  {
+    const navigate = useNavigate();
     return( 
-    <div className='reg-container'>
-        <form name= "reg-form" method= "POST">
-            <label>Hello</label>
-        </form>
-    </div>
+        <div className='login'>
+            <h1>Log In</h1>
+            <div className='login-container'>
+                <div className='login-card'>
+                    <form name= "login-form" method= "POST">
+                        <p>
+                            <label htmlFor="uname">Username: 
+                                <input type= "text" id="uname"/>
+                            </label>
+                        </p>
+
+                        <p>
+                            <label htmlFor="pwd">Password: 
+                                <input type= "password" id="pwd"/>
+                            </label>
+                        </p>
+
+                        <p>
+                            <input type = "submit" value = "Submit" onClick={
+                                () => {
+                                    navigate('/');
+                                }
+                            }></input>
+                            <input type = "reset" value = "Reset"></input>
+                        </p>
+            
+                    </form>
+                </div>    
+            </div>
+        </div>
     )
 }
 
